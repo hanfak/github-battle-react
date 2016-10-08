ee6d237d64632d844efc9115e4dd8abc76e09cd1
Hanif Fakira <fakira.work@gmail.com>
2016-10-08 19:05:30 +0100

    function compositin using pure functions FP

--- app/index.js
+++ app/index.js
@@ -1,38 +1,35 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var ProfileFacebook = React.createClass({
-  render: function() {
+var ProfileFacebook = function(props) {
+  return (
+    <div>
+      <p>Facebook profile</p>
+      <a href={'https://www.fb.com/' + props.username}>
+      {props.username}
+      </a>
+    </div>
+  )
+}
+
+
+var ProfileLink = function(props) {
     return (
-      <div>
-        <p>Facebook profile</p>
-        <a href={'https://www.fb.com/' + this.props.username}>
-        {this.props.username}
-        </a>
-      </div>
-    )
-  }
-})
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <a href={'https://github.com/' + this.props.username}>
-        {this.props.username} Github profile
+      <a href={'https://github.com/' + props.username}>
+        {props.username} Github profile
       </a>
     )
   }
-})
 
-var Avatar = React.createClass({
-  render: function() {
+
+var Avatar = function(props) {
     return (
       <div>
-        <ProfileLink username={this.props.username} />
+        <ProfileLink username={props.username} />
         <br/>
-        <ProfileFacebook username={this.props.username1} />
+        <ProfileFacebook username={props.username1} />
       </div>
     )
   }
-})
 
 ReactDOM.render(<Avatar username="hanfak" username1="hanif.fakira" />, document.getElementById('app'));
