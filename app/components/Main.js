b911e50396da4282bce0fa52d5275fd11d43ef36
Hanif Fakira <fakira.work@gmail.com>
2016-10-09 11:55:30 +0100

    prompt container component + css
    -inline css, extract to var
    - create form

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,12 +3,26 @@ var Link = require('react-router').Link;
 
 var Main = React.createClass({
   render: function () {
+    var menuStyle = {
+      "padding-bottom": "50px",
+      "font-size": "20px"
+    };
+
     return (
-      <div className='main-container'>
-        <h1>Main - always here</h1>
-        <Link to="/">home</Link>
-        <br/>
-        <Link to="/guest">guest</Link>
+      <div className='main-container col-sm-12 text-center'>
+        <h1>Github Battle</h1>
+        <div style={menuStyle}>
+          <p><Link to="/" className="col-md-4">
+            home
+          </Link></p>
+          <p><Link to="/guest" className="col-md-4">
+            guest
+          </Link></p>
+          <p><Link to="/playerOne" className="col-md-4">
+            player one input
+          </Link></p>
+        </div>
+
         {this.props.children}
       </div>
     )
--- app/config/routes.js
+++ app/config/routes.js
@@ -8,12 +8,14 @@ var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
 var Guest = require("../components/Guest");
+var PromptContainer = require("../containers/PromptContainer");
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
       <Route path='/guest' component={Guest} />
+      <Route path="playerOne" component={PromptContainer} />
     </Route>
   </Router>
 );
