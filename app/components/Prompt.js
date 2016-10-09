93028d48c3932b7eddcccdd5299b603144a46faa
Hanif Fakira <fakira.work@gmail.com>
2016-10-09 14:35:05 +0100

    refactor UI extract to Prompts component
    - seperate container and presentation components
    - container should handle all logic, should render another componet that
      handles the ui/jsx

---
+++ app/components/Prompt.js
@@ -0,0 +1,34 @@
+var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+
+var Prompt = React.createClass({
+  render: function () {
+    return (
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
+        <h2>Choose {this.props.route.header} </h2>
+        <div className="col-sm-12">
+          <form onSubmit={this.onSubmitUser}>
+            <div className="form-group">
+              <input
+                autoFocus
+                className="form-control"
+                placeholder="Github Username"
+                type="text"
+                onChange={this.onUpdateUser}
+                value={this.state.username}/>
+            </div>
+            <div className="form-group col-sm-4 col-sm-offset-4">
+              <button
+                className="btn btn-block btn-success"
+                type="submit">
+                  Continue
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = Prompt;
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
+
 
 var PromptContainer = React.createClass ({
   contextTypes: {
@@ -16,14 +17,14 @@ var PromptContainer = React.createClass ({
     console.log(event);
     this.setState({
       username: event.target.value
-    })
+    });
   },
 
   onSubmitUser: function (event) {
     event.preventDefault();
     var username = this.state.username;
     console.log(username);
-    console.log(this.props)
+    console.log(this.props);
     this.setState({
       username: ''
     });
@@ -34,7 +35,7 @@ var PromptContainer = React.createClass ({
           playerOne: this.props.routeParams.playerOne,
           playerTwo: username,
         }
-      })
+      });
     }
     else {
       this.context.router.push('/playerTwo/' + this.state.username)
@@ -43,29 +44,7 @@ var PromptContainer = React.createClass ({
 
   render: function () {
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h2>Choose {this.props.route.header} </h2>
-        <div className="col-sm-12">
-          <form onSubmit={this.onSubmitUser}>
-            <div className="form-group">
-              <input
-                autoFocus
-                className="form-control"
-                placeholder="Github Username"
-                type="text"
-                onChange={this.onUpdateUser}
-                value={this.state.username}/>
-            </div>
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                  Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt />
     );
   }
 });
